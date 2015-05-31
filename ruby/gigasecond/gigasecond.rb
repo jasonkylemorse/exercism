2707f818100e102d6bc085fa3710fc6c3184a637
jasonkylemorse <jasonkylemorse@gmail.com>
2015-05-31 19:59:38 -0400

    gigasecond solution

---
+++ ruby/gigasecond/gigasecond.rb
@@ -0,0 +1,23 @@
+# assignment 3: gigasecond
+# author: Jason Morse
+# date: May 31, 2015
+# 
+
+class Gigasecond
+
+	VERSION = 1
+
+	def self.from(birthday)
+
+		# returns a floating point number of seconds since the Epoch
+		t = birthday
+		"%10.5f" % t.to_f
+
+		gs_anniversary = t + ((10 ** 9))
+		date = Time.at(gs_anniversary)
+
+		return date
+
+	end
+
+end
