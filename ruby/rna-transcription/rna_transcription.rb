9545eae23438d816b2d7311d1a1272b105719c5c
jasonkylemorse <jasonkylemorse@gmail.com>
2015-05-31 20:55:46 -0400

    rna-transcription solution

---
+++ ruby/rna-transcription/rna_transcription.rb
@@ -0,0 +1,54 @@
+# assignment 4: RNA transcription
+# author: Jason Morse
+# date: May 31, 2015
+# 
+
+class Complement
+
+	def self.of_dna(dna)
+		rna = ''
+
+		dna.each_char do |nucleotide|
+			case nucleotide
+			when 'G'
+				rna << 'C'
+			when 'C'
+				rna << 'G'
+			when 'T'
+				rna << 'A'
+			when 'A'
+				rna << 'U'
+			else
+				raise ArgumentError.new("Invalid input")
+			end
+
+		end
+
+		return rna
+
+	end
+
+	def self.of_rna(rna)
+		dna = ''
+
+		rna.each_char do |nucleotide|
+			case nucleotide
+			when 'C'
+				dna << 'G'
+			when 'G'
+				dna << 'C'
+			when 'A'
+				dna << 'T'
+			when 'U'
+				dna << 'A'
+			else
+				raise ArgumentError.new("Invalid input")
+			end
+
+		end
+
+		return dna
+
+	end
+
+end
