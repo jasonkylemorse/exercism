eac75bfc05ed0a67f123d216934066711ab188d9
jasonkylemorse <jasonkylemorse@gmail.com>
2015-06-02 20:43:21 -0400

    refactored rna_transcription

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,33 +1,24 @@
 # assignment 4: RNA transcription
 # author: Jason Morse
-# date: May 31, 2015; updated: June 1, 2015
+# date: May 31, 2015; updated: June 2, 2015
 # 
 
 class Complement
 
-  attr_accessor :dna_translations
-  attr_accessor :rna_translations
+  DNA_TRANS = { 'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U' }
+  RNA_TRANS = DNA_TRANS.invert
 
   def self.of_dna(dna)
-    @dna_translations = { 'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U' }
 
-    dna.each_char do |nucleotide|
-      raise ArgumentError, 'Invalid input' if !@dna_translations.has_key?(nucleotide)
-    end
-
-    dna.split('').map {|nucleotide| @dna_translations[nucleotide]}.join
+    raise ArgumentError, 'Invalid input' unless dna.chars.all? { |nucleotide| DNA_TRANS.has_key?(nucleotide) }
+    dna.chars.map {|nucleotide| DNA_TRANS[nucleotide]}.join
 
   end
 
   def self.of_rna(rna)
-    #@rna_translations = @dna_translations.invert <-- sometimes 'undefined method'
-    @rna_translations = { 'C' => 'G', 'G' => 'C', 'A' => 'T', 'U' => 'A' }
 
-    rna.each_char do |nucleotide|
-      raise ArgumentError, 'Invalid input' if !@rna_translations.has_key?(nucleotide)
-    end
-
-    rna.split('').map {|nucleotide| @rna_translations[nucleotide]}.join
+    raise ArgumentError, 'Invalid input' unless rna.chars.all? { |nucleotide| RNA_TRANS.has_key?(nucleotide) }
+    rna.chars.map {|nucleotide| RNA_TRANS[nucleotide]}.join
 
   end
 
