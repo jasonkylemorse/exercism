be9d285e062b86efd3422f8371530dd810ab2500
jasonkylemorse <jasonkylemorse@gmail.com>
2015-06-06 23:57:26 -0400

    word count solution

---
+++ ruby/word-count/word_count.rb
@@ -0,0 +1,22 @@
+# assignment: word count
+# author: Jason Morse
+# date: June 6, 2015
+# 
+
+class Phrase
+
+  def initialize(phrase)
+    @phrase = phrase
+  end
+
+  def word_count
+    count = {}
+    words = @phrase.downcase.scan(/[a-zA-Z0-9]+[']*[a-zA-Z0-9]*/)
+    words.each do |word| 
+      count[word] += 1 if count.has_key?(word)
+      count.merge!(word => 1) if !count.has_key?(word)
+    end
+    count 
+  end
+
+end
