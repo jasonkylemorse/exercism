5ac14f785e2177464d660c57eb4c51e1be339bce
jasonkylemorse <jasonkylemorse@gmail.com>
2015-06-07 19:16:46 -0400

    refactored word count

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -12,10 +12,7 @@ class Phrase
   def word_count
     count = {}
     words = @phrase.downcase.scan(/[a-z0-9]+[']*[a-z0-9]*/)
-    words.each do |word| 
-      count[word] += 1 if count.has_key?(word)
-      count.merge!(word => 1) if !count.has_key?(word)
-    end
+    words.each { |word| count.has_key?(word) ? count[word] += 1 : count.merge!(word => 1) }
     count 
   end
 
