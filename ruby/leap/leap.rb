d5728dcfd3a81f1f0ec96cd04bee9f7f652745b5
jasonkylemorse <jasonkylemorse@gmail.com>
2015-06-13 00:44:55 -0400

    refactored leap

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -1,13 +1,12 @@
-# assignment: leap years
+# assignment 10: leap
 # author: Jason Morse
-# date: June 6, 2015
-# 
+# date: June 6, 2015; updated: June 12, 2015
+#
 
 class Year
 
   def self.leap?(year)
-    true if (year % 4 == 0 && year % 100 != 0) || 
-      (year % 4 == 0 && year % 100 == 0 && year % 400 == 0)
+    true if (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
   end
 
 end
