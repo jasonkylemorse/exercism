5cb5087147bc2d6dc33e60dadac20375db04d14d
jasonkylemorse <jasonkylemorse@gmail.com>
2015-06-12 23:51:20 -0400

    fixed style

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,20 +1,17 @@
-# assignment: raindrops
+# assignment 5: raindrops
 # author: Jason Morse
-# date: May 31, 2015; updated: June 5, 2015
-# 
+# date: May 31, 2015; updated: June 12, 2015
+#
 
 class Raindrops
 
   def self.convert(number)
-    output = ""
-    
-    output << "Pling" if number % 3 == 0
-    output << "Plang" if number % 5 == 0
-    output << "Plong" if number % 7 == 0
-    output << number.to_s if output == ""
-    
+    output = ''
+    output << 'Pling' if number % 3 == 0
+    output << 'Plang' if number % 5 == 0
+    output << 'Plong' if number % 7 == 0
+    output << number.to_s if output == ''
     output
-
   end
 
 end
