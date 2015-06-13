21de517404d3f052232c9d01701697b61b5e3567
jasonkylemorse <jasonkylemorse@gmail.com>
2015-06-13 00:28:59 -0400

    refactored robot name

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -1,17 +1,16 @@
-# assignment: robot name
+# assignment 8: robot-name
 # author: Jason Morse
-# date: June 4, 2015; updated: June 5, 2015
-# 
+# date: June 4, 2015; updated: June 12, 2015
+#
 
 class Robot
 
-  def initialize
-    @name = ('A'..'Z').to_a.sample(2).join << (0..9).to_a.sample(3).join
+  def generate_name
+    ('A'..'Z').to_a.sample(2).join << (0..9).to_a.sample(3).join
   end
 
   def name
-    initialize if @name == nil
-    @name
+    @name ||= generate_name
   end
 
   def reset
