64c05afd5a9a1cda963a50e5e205760a0c5563df
jasonkylemorse <jasonkylemorse@gmail.com>
2015-06-13 01:05:43 -0400

    fixed robot name

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -1,12 +1,12 @@
 # assignment 8: robot-name
 # author: Jason Morse
-# date: June 4, 2015; updated: June 12, 2015
+# date: June 4, 2015; updated: June 13, 2015
 #
 
 class Robot
 
   def generate_name
-    ('A'..'Z').sample(2).join << (0..9).to_a.sample(3).join
+    ('A'..'Z').to_a.sample(2).join << (0..9).to_a.sample(3).join
   end
 
   def name
