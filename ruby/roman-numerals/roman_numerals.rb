c690e67705eefc6c9bee56d17e5efcae8fa43275
jasonkylemorse <jasonkylemorse@gmail.com>
2015-06-12 23:54:22 -0400

    fixed style

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,29 +1,25 @@
-# assignment: roman numerals
+# assignment 7: roman-numerals
 # author: Jason Morse
-# date: June 4, 2015
-# 
+# date: June 4, 2015; updated: June 12, 2015
+#
 
 class Fixnum
 
-  SPECIAL_CASES = { 1 => "I", 4 => "IV", 5 => "V", 9 => "IX", 10 => "X", 40 => "XL", 50 => "L", 90 => "XC", 100 => "C", 400 => "CD", 500 => "D", 900 => "CM", 1000 => "M" }
+  SPECIAL_CASES = { 1 => 'I', 4 => 'IV', 5 => 'V', 9 => 'IX', 10 => 'X',
+                    40 => 'XL', 50 => 'L', 90 => 'XC', 100 => 'C', 400 => 'CD',
+                    500 => 'D', 900 => 'CM', 1000 => 'M' }
 
   def to_roman
-
     number = self
-    result = ""
+    result = ''
 
     while number > 0
-      selection = SPECIAL_CASES.select { |k, v| k <= number }.keys.max
+      selection = SPECIAL_CASES.select { |k, _| k <= number }.keys.max
       result << SPECIAL_CASES[selection]
       number -= selection
     end
 
     result
-
   end
 
 end
-
-# don't know exactly why this works yet -> instructions taken from here
-# http://www.rapidtables.com/convert/number/how-number-to-roman-numerals.htm 
-
