a29d988b833bf244b08c2db1c09ea86c41b1efef
jasonkylemorse <jasonkylemorse@gmail.com>
2015-06-14 19:32:40 -0400

    refactored rna transcription

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,19 +1,21 @@
 # assignment 4: rna-transcription
 # author: Jason Morse
-# date: May 31, 2015; updated: June 12, 2015
+# date: May 31, 2015; updated: June 14, 2015
 #
 
 class Complement
 
   def self.of_dna(dna)
-    fail ArgumentError, 'Invalid' unless
-      dna.each_char.all? { |nucleotide| 'GCTA'.include?(nucleotide) }
+    unless dna.each_char.all? { |nucleotide| 'GCTA'.include?(nucleotide) }
+      fail ArgumentError, "Nucleotide #{nucleotide} is not valid input (GCTA)"
+    end
     dna.tr('GCTA', 'CGAU')
   end
 
   def self.of_rna(rna)
-    fail ArgumentError, 'Invalid' unless
-      rna.each_char.all? { |nucleotide| 'CGAU'.include?(nucleotide) }
+    unless rna.each_char.all? { |nucleotide| 'CGAU'.include?(nucleotide) }
+      fail ArgumentError, "Nucleotide #{nucleotide} is not valid input (CGAU)"
+    end
     rna.tr('CGAU', 'GCTA')
   end
 
