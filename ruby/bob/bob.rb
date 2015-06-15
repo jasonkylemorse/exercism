d3bff821469ccf8057a74aa7e0e5a9a2516b8b41
jasonkylemorse <jasonkylemorse@gmail.com>
2015-06-14 20:25:30 -0400

    refactored bob

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,25 +1,21 @@
-# assignment: bob
+# assignment 13: bob
 # author: Jason Morse
 # date: June 10, 2015
-# 
-
-# all test cases pass but this can definitely be more concise (regex?)
-
+#
 
 class Bob
 
   def hey(remark)
-
     case
-    when (remark[-1] == '?' && remark != remark.upcase) || (remark[-1] == '?' && !remark.gsub(/\W+/, '').chars.any? { |char| char.between?('a', 'z') || char.between?('A', 'Z') })
-      'Sure.'
-    when remark == remark.upcase && !remark.strip.empty? && remark.gsub(/\W+/, '').chars.any? { |char| char.between?('a', 'z') || char.between?('A', 'Z') }
+    when remark == remark.upcase && remark != remark.downcase
       'Whoa, chill out!'
+    when remark[-1] == '?'
+      'Sure.'
     when remark.strip.empty?
       'Fine. Be that way!'
     else
       'Whatever.'
     end
-
   end
+
 end
