efe01f3a12216bdd12df8d5ff6b05eedc05323b1
jasonkylemorse <jasonkylemorse@gmail.com>
2015-07-09 13:30:35 -0400

    rubocopped

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -12,35 +12,35 @@ class SpaceAge
   end
 
   def on_earth
-    @seconds/31_557_600.to_f
+    @seconds / 31_557_600.to_f
   end
 
-  def on_mercury 
-    @seconds/(31_557_600.to_f * 0.2408467)
+  def on_mercury
+    @seconds / (31_557_600.to_f * 0.2408467)
   end
 
   def on_venus
-    @seconds/(31_557_600.to_f * 0.61519726)
+    @seconds / (31_557_600.to_f * 0.61519726)
   end
 
   def on_mars
-    @seconds/(31_557_600.to_f * 1.8808158)
+    @seconds / (31_557_600.to_f * 1.8808158)
   end
 
   def on_jupiter
-    @seconds/(31_557_600.to_f * 11.862615)
+    @seconds / (31_557_600.to_f * 11.862615)
   end
 
   def on_saturn
-    @seconds/(31_557_600.to_f * 29.447498)
+    @seconds / (31_557_600.to_f * 29.447498)
   end
 
   def on_uranus
-    @seconds/(31_557_600.to_f * 84.016846)
+    @seconds / (31_557_600.to_f * 84.016846)
   end
 
   def on_neptune
-    @seconds/(31_557_600.to_f * 164.79132)
+    @seconds / (31_557_600.to_f * 164.79132)
   end
 
 end
